0b80e598630a6c01fd704d9a366f54ea47dc4524
devops-itcs <83263728+devops-itcs@users.noreply.github.com>
2021-12-02 10:51:10 +0700

    Update update.py

--- update.py
+++ update.py
@@ -3,7 +3,7 @@
 import paramiko
 import psycopg2
 
-server = "10.10.92.110"
+server = "10.10.92.115"
 
 conn = psycopg2.connect(
    database="opensips", user='postgres', password='postgres', host=server, port= '5432'
